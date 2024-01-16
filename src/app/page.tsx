ab9e6a916a069531edd44a81806fd6821fa4d42a
Vitor Domith <vcdomith@hotmail.com>
2024-01-16 18:28:01 -0300

    First Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,67 @@
+'use client'
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import { ChangeEvent, useEffect, useState } from 'react'
+import { get } from 'http'
+import { text } from 'stream/consumers'
+
 
 export default function Home() {
+
+  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('#fff')
+  const [textColor, setTextColor] = useState('#141414')
+
+  const changeColors = (evento: ChangeEvent<HTMLInputElement>) => {
+
+    setColor(evento.target.value)
+
+  }
+
+  function getContrastYIQ(hexcolor: string){
+    hexcolor = hexcolor.slice(1)
+
+    var r = parseInt(hexcolor.substr(0,2),16);
+    var g = parseInt(hexcolor.substr(2,2),16);
+    var b = parseInt(hexcolor.substr(4,2),16);
+    var yiq = ((r*299)+(g*587)+(b*114))/1000;
+    return (yiq >= 128) ? '#141414' : '#ffffff';
+  }
+
+  useEffect(() => {
+
+    // console.log(getContrastYIQ(color.slice(1)), color)
+    setTextColor(getContrastYIQ(color))
+
+  }, [color])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+    <>
+    
+      <h1>Click on the button to count up</h1>
+      <h2>{count}</h2>
+      <div
+        style={{
+          width: '300px', 
+          height: '300px',
+          padding: '1rem',
+          backgroundColor: `${color}`
+        }}
+      >
+        <p
+          style={{
+            color: `${textColor}`
+          }}
+        >
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero laudantium mollitia dicta tempore aspernatur fuga totam sint maxime optio nam qui eligendi, quaerat dolorum reiciendis. Quo possimus itaque atque sint?
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
+      <input 
+        type="color" 
+        onChange={changeColors}
+      />
+    </>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }

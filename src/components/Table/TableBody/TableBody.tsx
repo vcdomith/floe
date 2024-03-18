570cb8ff5e2bc2bf0f0f80a8d67b9cd319a82837
Vitor Domith <vcdomith@hotmail.com>
2024-03-18 16:12:14 -0300

    spring implementation test - wip

--- src/components/Table/TableBody/TableBody.tsx
+++ src/components/Table/TableBody/TableBody.tsx
@@ -6,6 +6,7 @@ import { IProduto } from '@/interfaces/IProduto'
 import { Dispatch, MouseEvent, SetStateAction, TransitionEvent, use, useEffect, useRef, useState } from 'react'
 import Converter from '@/utils/typeConversion'
 import { get } from 'http'
+import { animated, useTransition } from '@react-spring/web'
 
 interface TableBodyProps {
 
@@ -33,14 +34,6 @@ const TableBody = ({ controleProdutos, setControleProdutos, setFatores, setValor
     const { searchParam, sorted } = filtros
 
     let displayControl = Array(controleProdutos.length).fill(false)
-
-    // const [fatoresDisplay, setFatoresDisplay] = useState<boolean[]>(displayControl)
-
-    // const getIndex = (id: number): number => {
-
-    //     return controleProdutos.findIndex(produto => produto.id === id)
-
-    // }
 
     function calcularTabela(valor: number, args: number[]): number {
 
@@ -176,6 +169,16 @@ const TableBody = ({ controleProdutos, setControleProdutos, setFatores, setValor
 
     const produtoAtivo = fatoresDisplay.includes(true)
 
+    const transitions = useTransition(
+        aplicarFiltros(controleProdutos).map(({ id }, index) => ({ id, index })), {
+            key: ({ id }: { id: string }) => id,
+          from: { opacity: 0, height: 0 },
+          enter: { opacity: 1, height: 'auto' },
+          leave: { opacity: 0, height: 0, zIndex: -2 },
+          config: { tension: 220, friction: 20 }
+        }
+    )
+
     useEffect(() => {
 
         if (controleProdutos.length !== fatoresDisplay.length)
@@ -183,6 +186,7 @@ const TableBody = ({ controleProdutos, setControleProdutos, setFatores, setValor
 
             
     }, [controleProdutos, fatoresDisplay.length, setFatoresDisplay])
+
 
   return(
     
@@ -196,14 +200,11 @@ const TableBody = ({ controleProdutos, setControleProdutos, setFatores, setValor
         {(aplicarFiltros(controleProdutos).length > 0) 
 
         ?
-        aplicarFiltros(controleProdutos).map(({ id }, index) => 
-        
-            <div  
+        transitions((style, { id, index }) => 
+            <animated.div  
                 className={`tr`}
-                // onClick={() => console.log(aplicarFiltros(controleProdutos))}
-                // onClick={() => console.log(fatoresDisplay.includes(true))}
-                onClick={() => console.log(produtosFiltrados)}
-                key={(index*3.1415)}
+                style={style}
+                key={id}
             >
                 {getTabelas(getIndex(id)).map((valor: string | number, index: number) => 
                     <div 
@@ -267,8 +268,81 @@ const TableBody = ({ controleProdutos, setControleProdutos, setFatores, setValor
 
             </>
                 
-            </div>
+            </animated.div>
         )
+        // aplicarFiltros(controleProdutos).map(({ id }, index) => 
+        
+        //     <div  
+        //         className={`tr`}
+        //         // onClick={() => console.log(aplicarFiltros(controleProdutos))}
+        //         // onClick={() => console.log(fatoresDisplay.includes(true))}
+        //         onClick={() => console.log(produtosFiltrados)}
+        //         key={(index*3.1415)}
+        //     >
+        //         {getTabelas(getIndex(id)).map((valor: string | number, index: number) => 
+        //             <div 
+        //                 className='td'
+        //                 key={index}
+        //             >{
+        //                 (typeof valor === 'number')
+        //                 ? valor.toLocaleString('pt-br', {minimumFractionDigits: 2, maximumFractionDigits: 4})
+        //                 : valor
+        //             }
+        //             </div>
+        //         )}
+        //     <>
+            
+        //     <button>
+        //         <svg 
+        //             onClick={() => mostrarFatores(index)}
+        //             width="25px" 
+        //             height="25px" 
+        //             viewBox="0 0 32 32" 
+        //             xmlns="http://www.w3.org/2000/svg"
+        //         >
+        //                 <path 
+        //                     d="M6.001 7.128L6 10.438l19.998-.005L26 7.124zM6.001 21.566L6 24.876l19.998-.006.002-3.308zM6.001 14.341L6 17.65l19.998-.004.002-3.309z"
+        //                 />
+        //         </svg>
+        //     </button>
+            
+        //     <button>
+        //         <svg 
+        //             onClick={() => excluirLinha(getIndex(id))}
+        //             width="25px" 
+        //             height="25px" 
+        //             viewBox="0 0 32 32" 
+        //             xmlns="http://www.w3.org/2000/svg"
+        //         >
+        //                 <path 
+        //                     d="M7.004 23.087l7.08-7.081-7.07-7.071L8.929 7.02l7.067 7.069L23.084 7l1.912 1.913-7.089 7.093 7.075 7.077-1.912 1.913-7.074-7.073L8.917 25z"
+        //                 />
+        //         </svg>
+        //     </button>
+        //     <>
+        //     <section 
+        //         className='backdrop'
+        //         hidden={!fatoresDisplay[index]}
+        //         onClick={(e) => handleModalClick(index, e as any)}
+        //     >    
+        //     </section>
+        //     <FatoresTable
+        //         display={fatoresDisplay[index]}
+        //         fatores={controleProdutos[getIndex(id)].fatores}
+        //         setFatores={setFatores(getIndex(id))}
+        //         valor={controleProdutos[getIndex(id)].unitario}
+        //         setValor={setValor(getIndex(id))}
+        //         handleSubmit={e => {
+        //             e.preventDefault()
+        //             mostrarFatores(index)
+        //         }}
+        //     />
+        //     </>
+
+        //     </>
+                
+        //     </div>
+        // )
         
         : 
         <div>

243c921ca334e72e8efb2d036659ac0513ca6d67
Vitor Domith <vcdomith@hotmail.com>
2024-05-13 18:10:15 -0300

    Final Dia

--- src/app/(app)/(nav)/Nav.tsx
+++ src/app/(app)/(nav)/Nav.tsx
@@ -40,13 +40,13 @@ const Nav = ({ pathname }: { pathname: string}) => {
             <Link href='/configurar' prefetch>
                         <button className={`${(pathname === '/configurar' ? style.active : '')}`}>
                             <svg width="500" height="500" viewBox="0 0 500 500" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="355" cy="256.614" r="68" stroke="#E8D4B0" stroke-width="40"/>
-                                <path d="M271 258H0" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                                <path d="M500 258H441" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                                <path d="M69.9496 0.571302C69.9497 26.7286 115.604 30.0417 115.4 68.9252C115.197 107.809 21 126.666 21 164.663C21 200.009 69.9497 216.388 69.9496 237.391" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                                <path d="M241.452 262.571C241.451 288.729 195.797 292.042 196.001 330.925C196.205 369.809 290.401 388.666 290.401 426.663C290.401 462.009 241.451 478.388 241.452 499.391" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                                <path d="M183.95 0.571302C183.95 26.7286 229.604 30.0417 229.4 68.9252C229.197 107.809 135 126.666 135 164.663C135 200.009 183.95 216.388 183.95 237.391" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                                <path d="M131.452 262.571C131.451 288.729 85.7968 292.042 86.0007 330.925C86.2046 369.809 180.401 388.666 180.401 426.663C180.401 462.009 131.451 478.388 131.452 499.391" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
+                                <circle cx="355" cy="256.614" r="68" stroke="#E8D4B0" strokeWidth="40"/>
+                                <path d="M271 258H0" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                                <path d="M500 258H441" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                                <path d="M69.9496 0.571302C69.9497 26.7286 115.604 30.0417 115.4 68.9252C115.197 107.809 21 126.666 21 164.663C21 200.009 69.9497 216.388 69.9496 237.391" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                                <path d="M241.452 262.571C241.451 288.729 195.797 292.042 196.001 330.925C196.205 369.809 290.401 388.666 290.401 426.663C290.401 462.009 241.451 478.388 241.452 499.391" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                                <path d="M183.95 0.571302C183.95 26.7286 229.604 30.0417 229.4 68.9252C229.197 107.809 135 126.666 135 164.663C135 200.009 183.95 216.388 183.95 237.391" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                                <path d="M131.452 262.571C131.451 288.729 85.7968 292.042 86.0007 330.925C86.2046 369.809 180.401 388.666 180.401 426.663C180.401 462.009 131.451 478.388 131.452 499.391" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
                             </svg>
                             <p>Configurar Fatores</p>
                         </button>
@@ -54,13 +54,13 @@ const Nav = ({ pathname }: { pathname: string}) => {
                     <Link href='/tabela' prefetch>
                         <button className={`${(pathname === '/tabela' ? style.active : '')}`}>
                         <svg width="500" height="500" viewBox="0 0 500 500" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="144" cy="255.844" r="69" stroke="#E8D4B0" stroke-width="40"/>
-                            <path d="M148 168C148 131.5 123.141 103.729 148 80C171.729 57.3496 197.5 80 232 80" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                            <path d="M148 344C148 368 128.493 397.152 148 419C169.848 443.47 206 419 232 419" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                            <path d="M84.5 257.5H0" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                            <path d="M500 80.0012C477.075 79.8807 466 125.704 422 125.5C378 125.296 358 31 315 31C275 31 251.458 79.5102 232 80.0012" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                            <path d="M500 419.001C477.075 418.881 466 464.704 422 464.5C378 464.296 358 370 315 370C275 370 251.458 418.51 232 419.001" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                            <path d="M500 259.001C477.075 258.881 466 304.704 422 304.5C378 304.296 358 210 315 210C275 210 251.458 258.51 232 259.001" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
+                            <circle cx="144" cy="255.844" r="69" stroke="#E8D4B0" strokeWidth="40"/>
+                            <path d="M148 168C148 131.5 123.141 103.729 148 80C171.729 57.3496 197.5 80 232 80" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                            <path d="M148 344C148 368 128.493 397.152 148 419C169.848 443.47 206 419 232 419" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                            <path d="M84.5 257.5H0" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                            <path d="M500 80.0012C477.075 79.8807 466 125.704 422 125.5C378 125.296 358 31 315 31C275 31 251.458 79.5102 232 80.0012" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                            <path d="M500 419.001C477.075 418.881 466 464.704 422 464.5C378 464.296 358 370 315 370C275 370 251.458 418.51 232 419.001" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                            <path d="M500 259.001C477.075 258.881 466 304.704 422 304.5C378 304.296 358 210 315 210C275 210 251.458 258.51 232 259.001" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
                         </svg>
                             <p>Calcular Tabela</p>
                         </button>
@@ -88,10 +88,10 @@ const Nav = ({ pathname }: { pathname: string}) => {
                 <Link href='/usuario' prefetch>
                     <button className={`${(pathname === '/cadastros' ? style.active : '')}`}>
                         <svg width="500" height="500" viewBox="0 0 500 500" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M319 250.138C319 287.77 288.108 318.276 250 318.276C211.892 318.276 181 287.77 181 250.138C181 212.506 211.892 182 250 182C288.108 182 319 212.506 319 250.138Z" stroke="#E8D4B0" stroke-width="40"/>
-                            <path d="M480 250C480 377.025 377.025 480 250 480C122.975 480 20 377.025 20 250C20 122.975 122.975 20 250 20C377.025 20 480 122.975 480 250Z" stroke="#E8D4B0" stroke-width="40"/>
-                            <path d="M66 389.5C101.5 285.069 72.0802 294.379 169.5 268" stroke="#E8D4B0" stroke-width="40"/>
-                            <path d="M434.5 389.5C399 285.069 428.42 294.379 331 268" stroke="#E8D4B0" stroke-width="40"/>
+                            <path d="M319 250.138C319 287.77 288.108 318.276 250 318.276C211.892 318.276 181 287.77 181 250.138C181 212.506 211.892 182 250 182C288.108 182 319 212.506 319 250.138Z" stroke="#E8D4B0" strokeWidth="40"/>
+                            <path d="M480 250C480 377.025 377.025 480 250 480C122.975 480 20 377.025 20 250C20 122.975 122.975 20 250 20C377.025 20 480 122.975 480 250Z" stroke="#E8D4B0" strokeWidth="40"/>
+                            <path d="M66 389.5C101.5 285.069 72.0802 294.379 169.5 268" stroke="#E8D4B0" strokeWidth="40"/>
+                            <path d="M434.5 389.5C399 285.069 428.42 294.379 331 268" stroke="#E8D4B0" strokeWidth="40"/>
                         </svg>
                         <p>Usuário Email</p>
                     </button>
@@ -99,11 +99,11 @@ const Nav = ({ pathname }: { pathname: string}) => {
                 <Link href='/usuario' prefetch>
                     <button className={`${(pathname === '/cadastros' ? style.active : '')}`}>
                         <svg width="500" height="500" viewBox="0 0 500 500" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M319 250.138C319 287.77 288.108 318.276 250 318.276C211.892 318.276 181 287.77 181 250.138C181 212.506 211.892 182 250 182C288.108 182 319 212.506 319 250.138Z" stroke="#E8D4B0" stroke-width="40"/>
-                            <path d="M33 476C68.37 303.607 71.937 311.546 169 268" stroke="#E8D4B0" stroke-width="40"/>
-                            <path d="M468 476C432.37 303.607 428.777 311.546 331 268" stroke="#E8D4B0" stroke-width="40"/>
-                            <path d="M459 52.0002C422.388 51.8816 408.769 114.701 338.5 114.5C268.231 114.299 262.172 52.0002 193.5 52.0002C129.619 52.0002 64.0749 101.74 33 102.223" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                            <path d="M459 151C422.388 150.882 408.769 213.701 338.5 213.5C268.231 213.299 262.172 151 193.5 151C129.619 151 64.0749 200.74 33 201.223" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
+                            <path d="M319 250.138C319 287.77 288.108 318.276 250 318.276C211.892 318.276 181 287.77 181 250.138C181 212.506 211.892 182 250 182C288.108 182 319 212.506 319 250.138Z" stroke="#E8D4B0" strokeWidth="40"/>
+                            <path d="M33 476C68.37 303.607 71.937 311.546 169 268" stroke="#E8D4B0" strokeWidth="40"/>
+                            <path d="M468 476C432.37 303.607 428.777 311.546 331 268" stroke="#E8D4B0" strokeWidth="40"/>
+                            <path d="M459 52.0002C422.388 51.8816 408.769 114.701 338.5 114.5C268.231 114.299 262.172 52.0002 193.5 52.0002C129.619 52.0002 64.0749 101.74 33 102.223" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                            <path d="M459 151C422.388 150.882 408.769 213.701 338.5 213.5C268.231 213.299 262.172 151 193.5 151C129.619 151 64.0749 200.74 33 201.223" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
                         </svg>
                         <p>Usuário Email</p>
                     </button>
--- src/app/(app)/configurar/page.tsx
+++ src/app/(app)/configurar/page.tsx
@@ -33,6 +33,9 @@ export default function Configurar() {
 
     const [focus, setFocus] = useState(false)
 
+    const [valid, setValid] = useState(false)
+    const [validation, setValidation] = useState(false)
+
     function capitalize(string: string):string {
 
         const captalizedFirstLetter = string[0].toUpperCase()
@@ -82,6 +85,20 @@ export default function Configurar() {
 
     }
 
+    const resetForm = () => {
+
+        setNomeFornecedor('')
+        setFatorBase('')
+        setFatorNormal('')
+        setFatorSt('')
+        setTransporte(true)
+        setSt(true)
+        setDesconto(false)
+        setIpi(false)
+        setUnitarioNota(false)
+
+    }
+
     const getFornecedores = async () => {
         
         try {
@@ -109,12 +126,6 @@ export default function Configurar() {
                 transition: 'width 600ms ease',
             }}
         >
-            <p
-                style={{
-                    fontSize: '1.5rem',
-
-                }}
-            >{JSON.stringify(cadastroFornecedor)}</p>
             <span style={{
                 display: 'flex',
                 gap: '1rem',
@@ -124,132 +135,157 @@ export default function Configurar() {
                 onClick={() => setFocus(false)}
                 name="fornecedor"
                 onSubmit={(e) => handleSubmit(e)}
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    border: '2px solid',
-                    flex: `${focus ? 2 : 8}`,
-                    transition: 'flex 1s ease-out',
-                    borderRadius: '1rem',
-                    padding: '2rem 4rem'
-                }}
+                onInvalid={() => setValid(false)}
             >
                 <span className={style.logo}>
                 <svg width="50" height="50" viewBox="0 0 500 500" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="355" cy="256.614" r="68" stroke="#E8D4B0" stroke-width="40"/>
-                    <path d="M271 258H0" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                    <path d="M500 258H441" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                    <path d="M69.9496 0.571302C69.9497 26.7286 115.604 30.0417 115.4 68.9252C115.197 107.809 21 126.666 21 164.663C21 200.009 69.9497 216.388 69.9496 237.391" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                    <path d="M241.452 262.571C241.451 288.729 195.797 292.042 196.001 330.925C196.205 369.809 290.401 388.666 290.401 426.663C290.401 462.009 241.451 478.388 241.452 499.391" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                    <path d="M183.95 0.571302C183.95 26.7286 229.604 30.0417 229.4 68.9252C229.197 107.809 135 126.666 135 164.663C135 200.009 183.95 216.388 183.95 237.391" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
-                    <path d="M131.452 262.571C131.451 288.729 85.7968 292.042 86.0007 330.925C86.2046 369.809 180.401 388.666 180.401 426.663C180.401 462.009 131.451 478.388 131.452 499.391" stroke="#E8D4B0" stroke-width="40" stroke-linejoin="round"/>
+                    <circle cx="355" cy="256.614" r="68" stroke="#E8D4B0" strokeWidth="40"/>
+                    <path d="M271 258H0" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                    <path d="M500 258H441" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                    <path d="M69.9496 0.571302C69.9497 26.7286 115.604 30.0417 115.4 68.9252C115.197 107.809 21 126.666 21 164.663C21 200.009 69.9497 216.388 69.9496 237.391" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                    <path d="M241.452 262.571C241.451 288.729 195.797 292.042 196.001 330.925C196.205 369.809 290.401 388.666 290.401 426.663C290.401 462.009 241.451 478.388 241.452 499.391" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                    <path d="M183.95 0.571302C183.95 26.7286 229.604 30.0417 229.4 68.9252C229.197 107.809 135 126.666 135 164.663C135 200.009 183.95 216.388 183.95 237.391" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
+                    <path d="M131.452 262.571C131.451 288.729 85.7968 292.042 86.0007 330.925C86.2046 369.809 180.401 388.666 180.401 426.663C180.401 462.009 131.451 478.388 131.452 499.391" stroke="#E8D4B0" strokeWidth="40" strokeLinejoin="round"/>
                 </svg>
                     <h1
                         style={{
                             width: 'min-content'
                         }}
-                    >Novo fornecedor:</h1>
+                    >Novo Fornecedor:</h1>
                 </span>
-                {/* <span 
-                    style={{
-                        display: 'inline-flex',
-                        alignItems: "center",
-                        gap: '1rem',
-                    }}
-                >
-                    <p>Fornecedor</p>
-                    <SelectFornecedor />
-                </span> */}
-                <span>
-                    <h3>Nome Fornecedor</h3>
-                    <input 
-                        type="text"
-                        placeholder="Nome Fornecedor" 
-                        required
-                        value={nomeFornecedor}
-                        onChange={(e) => setNomeFornecedor(e.target.value)}
-                    />
+              
+                <div>
+
+                    <span>
+                        <h3>Nome Fornecedor</h3>
+                        <input 
+                            type="text"
+                            placeholder="Nome Fornecedor" 
+                            required
+                            value={nomeFornecedor}
+                            onChange={(e) => setNomeFornecedor(e.target.value)}
+                        />
+                    </span>
+                    <span>
+                        {/* svg validação */}
+                        <p>{validation}</p>
+                    </span>
+                    
+                </div>
+
+                <div className={style.inputs}>
+                    
+                    <div className={style.fatores}>
+
+                        <h3>Fatores:</h3>
+                        <span className={style.fator}>
+                            <p>Fator Base</p>
+                            <NumberInput 
+                                placeholder={"ex: 1,00"} 
+                                valor={fatorBase} 
+                                setValor={setFatorBase}                        
+                            />
+                        </span>
+                        <span className={style.fator}>
+                            <p>Fator Normal</p>
+                            <NumberInput 
+                                placeholder={"Fator Normal"} 
+                                valor={fatorNormal} 
+                                setValor={setFatorNormal}                        
+                            />
+                        </span>
+                        <span className={style.fator}>
+                            <p>Fator ST</p>
+                            <NumberInput 
+                                placeholder={"Fator ST"} 
+                                valor={fatorSt} 
+                                setValor={setFatorSt}                        
+                            />
+                        </span>
+
+                    </div>
+                    
+                    <div className={style.config}>
+
+                        <h3>Configurações:</h3>
+
+                        <div className={style.inputs}>
+                            <span>
+                                <p>Usa Transporte?</p>
+                                <CheckBox
+                                    checked={transporte}
+                                    setChecked={setTransporte}
+                                />
+                            </span>
+                            <span>
+                                <p>Usa ST?</p>
+                                <CheckBox
+                                    checked={st}
+                                    setChecked={setSt}
+                                />
+                            </span>
+                            <span>
+                                <p>Usa Desconto?</p>
+                                <CheckBox
+                                    checked={desconto}
+                                    setChecked={setDesconto}
+                                />
+                            </span>
+                            <span>
+                                <p>Usa IPI?</p>
+                                <CheckBox
+                                    checked={ipi}
+                                    setChecked={setIpi}
+                                />
+                            </span>
+                            <span>
+                                <p>Usa unitário nota?</p>
+                                <CheckBox
+                                    checked={unitarioNota}
+                                    setChecked={setUnitarioNota}
+                                />
+                            </span>
+                        </div>
+
+                    </div>
+                </div>
+
+                <span className={style.buttons}>
+                    <button type='submit'>Salvar Fornecedor</button>
+                    <button type='reset' onClick={() => resetForm()}>Limpar</button>
                 </span>
-                <span>
-                    <h3>Fatores:</h3>
-                </span>
-                <span>
-                    <p>Fator Base</p>
-                    <NumberInput 
-                        placeholder={"ex: 1,00"} 
-                        valor={fatorBase} 
-                        setValor={setFatorBase}                        
-                     />
-                </span>
-                <span>
-                    <p>Fator Normal</p>
-                    <NumberInput 
-                        placeholder={"Fator Normal"} 
-                        valor={fatorNormal} 
-                        setValor={setFatorNormal}                        
-                     />
-                </span>
-                <span>
-                    <p>Fator ST</p>
-                    <NumberInput 
-                        placeholder={"Fator ST"} 
-                        valor={fatorSt} 
-                        setValor={setFatorSt}                        
-                     />
-                </span>
-                <span>
-                    <h3>Configurações:</h3>
-                </span>
-                <span>
-                    <p>Usa Transporte?</p>
-                    <CheckBox
-                        checked={transporte}
-                        setChecked={setTransporte}
-                    />
-                </span>
-                <span>
-                    <p>Usa ST?</p>
-                    <CheckBox
-                        checked={st}
-                        setChecked={setSt}
-                    />
-                </span>
-                <span>
-                    <p>Usa Desconto?</p>
-                    <CheckBox
-                        checked={desconto}
-                        setChecked={setDesconto}
-                    />
-                </span>
-                <span>
-                    <p>Usa IPI?</p>
-                    <CheckBox
-                        checked={ipi}
-                        setChecked={setIpi}
-                    />
-                </span>
-                <span>
-                    <p>Usa unitário nota?</p>
-                    <CheckBox
-                        checked={unitarioNota}
-                        setChecked={setUnitarioNota}
-                    />
-                </span>
-                <button type="submit">Enviar</button>
-                {/* <input type="submit" hidden/> */}
+
             </form>
             <div
                 onClick={() => setFocus(true)}
                 style={{
                     border: '2px solid',
-                    flex: `${focus ? 8 : 2 }`,
+                    width: '100%',
+                    // flex: `${focus ? 8 : 2 }`,
                     transition: 'flex 1s ease-out',
                     borderRadius: '1rem',
                     padding: '1rem'
                 }}
-            ></div>
+            >
+            <button
+                onClick={() => getFornecedores()}
+            >Carregar fornecedores</button>
+            {fornecedoresDB?.map(({nome, fatorBase, fatorNormal, fatorST, transporte, st, desconto, ipi, unitarioNota}) => 
+                <div key={nome} style={{ border: '2px solid' }}>
+                    <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>nome:</p><p style={{ margin: 0 }}>{nome}</p></span>
+                    <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>fatorBase:</p><p style={{ margin: 0 }}>{fatorBase}</p></span>
+                    <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>fatorNormal:</p><p style={{ margin: 0 }}>{fatorNormal}</p></span>
+                    <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>fatorST:</p><p style={{ margin: 0 }}>{fatorST}</p></span>
+                    <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>transporte:</p><p style={{ margin: 0 }}>{transporte ? 'Sim' : 'Não'}</p></span>
+                    <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>st:</p><p style={{ margin: 0 }}>{st ? 'Sim' : 'Não'}</p></span>
+                    <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>desconto:</p><p style={{ margin: 0 }}>{desconto ? 'Sim' : 'Não'}</p></span>
+                    <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>ipi:</p><p style={{ margin: 0 }}>{ipi ? 'Sim' : 'Não'}</p></span>
+                    <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>unitarioNota:</p><p style={{ margin: 0 }}>{unitarioNota ? 'Sim' : 'Não'}</p></span>
+                </div>
+            )}
+            </div>
             </span>
-            <div>
+            {/* <div>
                 <button
                     onClick={() => getFornecedores()}
                 >Carregar fornecedores</button>
@@ -266,7 +302,7 @@ export default function Configurar() {
                         <span style={{ display: 'flex'}}><p style={{ margin: 0 }}>unitarioNota:</p><p style={{ margin: 0 }}>{unitarioNota ? 'Sim' : 'Não'}</p></span>
                     </div>
                 )}
-            </div>
+            </div> */}
         </div>
     )
 

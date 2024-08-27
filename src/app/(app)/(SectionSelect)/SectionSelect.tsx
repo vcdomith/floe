6aafd7b0caabc4a70b370ea10c9db737897fc865
Vitor Domith <vcdomith@hotmail.com>
2024-08-27 16:46:06 -0300

    media queries and minor changes

--- src/app/(app)/(SectionSelect)/SectionSelect.tsx
+++ src/app/(app)/(SectionSelect)/SectionSelect.tsx
@@ -3,6 +3,9 @@ import { Dispatch, SetStateAction, useMemo, useState } from "react";
 
 import style from './SectionSelect.module.scss'
 import { useCalcular } from "../calcular/context/CalcularContext";
+import { usePathname } from "next/navigation";
+import { sectionsByPath } from "./(sectionsByPath)/sectionsByPath";
+import { toPath } from "lodash";
 
 export default function SectionSelect() {
 
@@ -12,10 +15,13 @@ export default function SectionSelect() {
         setCalcularSection: setSection ,
     } = useCalcular()
 
+    const path = usePathname().slice(1,)
+
     return (
         <span className={style.select}>
             <SelectFornecedor 
-                fornecedoresControle={sections} 
+                fornecedoresControle={(sectionsByPath[path as keyof typeof sectionsByPath])} 
+                // fornecedoresControle={sections} 
                 fornecedor={section} 
                 setFornecedor={setSection as Dispatch<SetStateAction<string>>}                
                 omitSearch
